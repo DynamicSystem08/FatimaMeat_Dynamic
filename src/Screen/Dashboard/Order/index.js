52dbbb3d44a8ffaf118aa8e2728370b345df1645
BilalMehfooz12 <bilalmehfooz253@gmail.com>
2022-12-11 22:29:19 +0500

    order

--- src/Screen/Dashboard/Order/index.js
+++ src/Screen/Dashboard/Order/index.js
@@ -11,7 +11,30 @@ import "./index.css"
 import { useDispatch, useSelector } from 'react-redux';
 import { logoutUser } from '../../../store/slices/userSlice';
 
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
 function Dashboard() {
+
+
+    function createData(name, calories, fat, carbs, protein) {
+        return { name, calories, fat, carbs, protein };
+    }
+
+    const rows = [
+        createData('Order Id', 159, 6.0, 24, 4.0),
+        createData('Customer Name', 237, 9.0, 37, 4.3),
+        createData('Delivery Address', 262, 16.0, 24, 6.0),
+        createData('Order Item', 305, 3.7, 67, 4.3),
+        createData('Order Date', 356, 16.0, 49, 3.9),
+    ];
+
 
     const navigate = useNavigate()
     const dispatch = useDispatch()
@@ -68,8 +91,8 @@ function Dashboard() {
                 <Grid item lg={1}></Grid>
 
 
-                <Grid item lg={8} style={{ paddingTop: "100px" }}>
-                    <Grid container style={{ justifyContent: "center", marginBottom: "20px" }}>
+                <Grid item lg={8}  xs={12} style={{ paddingTop: "50px" }}>
+                    {/* <Grid container style={{ justifyContent: "center", marginBottom: "20px" }}>
                         <Grid item lg={2} >
                             <h5>Order Id</h5>
                         </Grid>
@@ -85,9 +108,40 @@ function Dashboard() {
                         <Grid item lg={2}>
                             <h5>Order Date</h5>
                         </Grid>
-                    </Grid>
+                    </Grid> */}
 
-                    {reduxAllOrders ? reduxAllOrders.map((item) => {
+
+                    <TableContainer component={Paper} sx={{ maxHeight: 270, minHeight: 270 ,maxWidth:700,minWidth:300}}>
+                        <Table sx={{ minWidth: 650 }} aria-label="simple table" stickyHeader >
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell><b>Order Id</b></TableCell>
+                                    <TableCell align="right"><b>Customer Name</b></TableCell>
+                                    <TableCell align="right"><b>Delivery Address</b></TableCell>
+                                    <TableCell align="right"><b>Order Item</b></TableCell>
+                                    <TableCell align="right"><b>Order Date</b></TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {rows.map((row) => (
+                                    <TableRow
+                                        key={row.name}
+                                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                    >
+                                        <TableCell component="th" scope="row">
+                                            {row.name}
+                                        </TableCell>
+                                        <TableCell align="center">{row.calories}</TableCell>
+                                        <TableCell align="center">{row.fat}</TableCell>
+                                        <TableCell align="center">{row.carbs}</TableCell>
+                                        <TableCell align="center">{row.protein}</TableCell>
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+
+                    {/* {reduxAllOrders ? reduxAllOrders.map((item) => {
                         return <div>
                             <Grid container style={{ justifyContent: "center" }}>
                                 <Grid item lg={2} >
@@ -114,7 +168,7 @@ function Dashboard() {
                     })
                         :
                         <div>No Data Found</div>
-                    }
+                    } */}
                 </Grid>
 
 
